4a2ed6d0b278d8535d66322a30a4c82205981f7b
Stephanie Raymos <stephanieraymos@gmail.com>
2020-07-10 14:52:21 -0700

    Added comments to campsite.js

--- models/campsite.js
+++ models/campsite.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
+//This is making a shorthand to the mongoose.Schema function so we can refer to it as Schema:
 const Schema = mongoose.Schema;
 
+//Creating Schema:
 const campsiteSchema = new Schema({
     name: {
         type: String,
@@ -12,9 +14,14 @@ const campsiteSchema = new Schema({
         required: true
     }
 }, {
-    timestamps: true
+    timestamps: true //created at and updated at
 });
 
+//Creating Model: 
+//First argument: Capitalized and singular version of the collection you want to use for this model; Campsite for campsites collection
+//Second argument: Schema we want to use for this collection) 
+//Returns a constructor function (A de-sugared class)
+//Used to instantiate documents for mondoDB
 const Campsite = mongoose.model('Campsite', campsiteSchema);
 
 module.exports = Campsite;
