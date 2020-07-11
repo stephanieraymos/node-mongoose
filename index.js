87e8398ff120e95aa8ac298b6752fd67915ce926
Stephanie Raymos <stephanieraymos@gmail.com>
2020-07-10 17:04:49 -0700

    Added more comments + created another testCampsite

--- index.js
+++ index.js
@@ -18,25 +18,38 @@ connect.then(() => {
 
     console.log('Connected correctly to server');
 
-    //instantiate a new document
+    //Instantiating a new document:
+    //Campsite is the model we are instantiating
     const newCampsite = new Campsite({
         name: 'React Lake Campground',
         description: 'test'
     });
 
+    const testCampsite = new Campsite({
+      name: 'Test Campground',
+      description: 'test'
+  });
+
+    //Using the save method on the new document (a mongoose method that will save the document to the database)
+    //Returns a promise that will tell us whether the save operation failed or succeeded
     newCampsite.save()
     .then(campsite => {
         console.log(campsite);
-        return Campsite.find();
+        return Campsite.find(); //Finds all documents that are based on the Campsite Model. If successful: Returns all found docs in an array of objects
+    })
+    testCampsite.save()
+    .then(campsite => {
+        console.log(campsite);
+        return Campsite.find(); //Finds all documents that are based on the Campsite Model. If successful: Returns all found docs in an array of objects
     })
     .then(campsites => {
-        console.log(campsites);
+        console.log(campsites); //Logging array of objects to the console
         return Campsite.deleteMany();
     })
     .then(() => {
         return mongoose.connection.close();
     })
-    .catch(err => {
+    .catch(err => { //Catch any errors and console log the error messages
         console.log(err);
         mongoose.connection.close();
     });
