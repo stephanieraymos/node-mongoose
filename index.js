af372dd1f249e98d0a0b007136158f93cfb9a00e
Stephanie Raymos <stephanieraymos@gmail.com>
2020-07-10 17:14:23 -0700

    Mongoose create method

--- index.js
+++ index.js
@@ -9,48 +9,40 @@ const url = 'mongodb://localhost:27017/nucampsite';
 //Second argument is an object to set up options (setting these to deal with deprication warnings from the mongoDB node driver)
 //Connect method returns a promise; so I chained a .then method to it
 const connect = mongoose.connect(url, {
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+  useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 });
 
 connect.then(() => {
 
-    console.log('Connected correctly to server');
+  console.log('Connected correctly to server');
 
-    //Instantiating a new document:
-    //Campsite is the model we are instantiating
-    const newCampsite = new Campsite({
-        name: 'React Lake Campground',
-        description: 'test' //If this description is deleted; it would return an error since we set up descriptions as 'required'
-    });
-
-    const testCampsite = new Campsite({
-      name: 'Test Campground',
-      description: 'test'
+  //Instantiating a new document:
+  //Campsite is the model we are instantiating
+  //The create method returns a promise that resolves to the new document
+  Campsite.create({
+    name: 'React Lake Campground',
+    description: 'test' //If this description is deleted; it would return an error since we set up descriptions as 'required'
   });
 
-    //Using the save method on the new document (a mongoose method that will save the document to the database)
-    //Returns a promise that will tell us whether the save operation failed or succeeded
-    newCampsite.save()
+  Campsite.create({
+    name: 'Test Campground',
+    description: 'test'
+  })
     .then(campsite => {
-        console.log(campsite);
-        return Campsite.find(); //Finds all documents that are based on the Campsite Model. If successful: Returns all found docs in an array of objects
-    })
-    testCampsite.save()
-    .then(campsite => {
-        console.log(campsite);
-        return Campsite.find(); //Finds all documents that are based on the Campsite Model. If successful: Returns all found docs in an array of objects
+      console.log(campsite);
+      return Campsite.find(); //Finds all documents that are based on the Campsite Model. If successful: Returns all found docs in an array of objects
     })
     .then(campsites => {
-        console.log(campsites); //Logging array of objects to the console
-        return Campsite.deleteMany();
+      console.log(campsites); //Logging array of objects to the console
+      return Campsite.deleteMany();
     })
     .then(() => {
-        return mongoose.connection.close();
+      return mongoose.connection.close();
     })
     .catch(err => { //Catch any errors and console log the error messages
-        console.log(err);
-        mongoose.connection.close();
+      console.log(err);
+      mongoose.connection.close();
     });
 });
